ce652de2f3521031afae3280fa7299b343d1eb19
Daniel Estrada <daniel.nicholas.estrada@gmail.com>
2023-12-01 16:28:45 -0600

    form handler

--- js/main.js
+++ js/main.js
@@ -12,10 +12,53 @@ const getMovies = () => {
         })
 }
 
+const postMovie = (title, rating) => {
+     const newMovie = {
+          title,
+          rating
+     }
+     const body = JSON.stringify(newMovie);
+     const url = `http://localhost:3000/movies`;
+     const options = {
+          method: "POST",
+          headers: {
+               "Content-Type": "application/json"
+          },
+          body: body
+     };
+     return fetch (url,options).then(response => response.json())
+     .then(movies => {
+          return movies;
+     })
+}
+
+const formHandler = () => {
+     const formBtn = document.querySelector('#submit-movie');
+     formBtn.addEventListener('click', e=>{
+          e.preventDefault();
+          const titleValue = document.querySelector('#title-form').value;
+          console.log(titleValue);
+          const ratingValue = document.querySelector('#rating-form').value;
+          console.log(ratingValue);
+          postMovie(titleValue, ratingValue);
+          getMovies()
+          .then(movies => {
+               renderMovies(movies);
+          });
+     });
+};
+
 const renderMovies = (movies) => {
+     const movieCol = document.querySelector('.page-wrapper .col');
+     movieCol.innerHTML = `
+          <div id=\"loading-wrapper\">Loading...
+               <div class="spinner-border" role="status">
+                    <span class="visually-hidden">Loading...</span>
+               </div>               
+          </div>
+`;
      const loadingSpinner = document.querySelector('#loading-wrapper');
      loadingSpinner.remove();
-     const movieCol = document.querySelector('.page-wrapper .col');
      for (let movie of movies) {
           const movieCard = document.createElement('div');
           movieCard.innerHTML = `
@@ -27,15 +70,13 @@ const renderMovies = (movies) => {
      }
 }
 
-
-
-
-
 //MAIN
 (() => {
 getMovies()
      .then(movies => {
           renderMovies(movies);
      });
+formHandler();
+
 //ƒ json() { [native code] }
 })();
