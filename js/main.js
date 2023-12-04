fc43af787b58c92c8f433d2c01c7d6a0c51660ac
Daniel Estrada <daniel.nicholas.estrada@gmail.com>
2023-12-04 16:30:11 -0600

    edit genres

--- js/main.js
+++ js/main.js
@@ -68,11 +68,18 @@ const formHandler = () => {
      });
 };
 
-const handleMovieEdit = ({id, title, rating, poster}, movieCard) => {
+const handleMovieEdit = ({id, title, rating, poster, genre}, movieCard) => {
      movieCard.innerHTML = `
                <img src="../img/${poster}" className="card-img-top" alt="${title}">
                <div className="card-body">
                <span>Title:</span><input id="title-form${id}" class="edit-title" type="text" name="title" value="${title}" placeholder="Movie Title">
+               <span>Genre:<select id="genre-submit" type="genre">
+                    <option>family</option>
+                    <option>sci-fi</option>
+                    <option>comedy</option>
+                    <option>drama</option>
+                    <option>horror</option>
+               </select></span>
                <span>Rating:</span><input id="rating-form${id}" class="edit-rating" type="text" name="rating" value="${rating}" placeholder="Movie Rating"><br>
                <button class="submit"><img src="../img/bandaid.svg"</button>
                <button class="delete"><img src="../img/trash-fill.svg"</button>
@@ -83,10 +90,12 @@ const handleMovieEdit = ({id, title, rating, poster}, movieCard) => {
           /// run patch method
           const editTitle = movieCard.querySelector('.edit-title').value;
           const editRating = movieCard.querySelector('.edit-rating').value;
+          const editGenre = movieCard.querySelector('#genre-submit').value;
           const editedMovie = {
                id: id,
                title: editTitle,
                rating: editRating,
+               genre: editGenre,
                poster: poster,
           }
           patchMovie(id, editedMovie);
@@ -94,18 +103,18 @@ const handleMovieEdit = ({id, title, rating, poster}, movieCard) => {
                <img src="../img/${poster}" className="card-img-top" alt="${title}">
                <div className="card-body">
                <h5 className="card-title">${editTitle}</h5>
-               <p className="card-text">${editRating}</p>
+               <p>${editGenre}</p><span className="card-text">${editRating}/10</span>
                <button class="edit"><img src="../img/bandaid-fill.svg"></button><button class="delete"><img src="../img/trash-fill.svg"</button>
                </div>
           `;
           const editBtn = movieCard.querySelector("button.edit");
           editBtn.addEventListener("click", e=>{
-               handleMovieEdit({id, title, rating, poster}, movieCard);
+               handleMovieEdit({id, title, rating, poster, genre}, movieCard);
           })
      });
 }
 
-const renderMovie = ({id, title, rating, poster}) => {
+const renderMovie = ({id, title, rating, poster, genre}) => {
      const movieCol = document.querySelector('.page-wrapper .movie-wrapper');
      const movieCard = document.createElement('div');
      movieCard.classList.add('card');
@@ -113,23 +122,23 @@ const renderMovie = ({id, title, rating, poster}) => {
           <img src="../img/${poster}" className="card-img-top" alt="${title}">
           <div className="card-body">
                <h5 className="card-title">${title}</h5>
-               <p className="card-text">${rating}</p>
+               <p>${genre}</p><span className="card-text">${rating}/10</span>
                <button class="edit"><img src="../img/bandaid-fill.svg"></button><button class="delete"><img src="../img/trash-fill.svg"</button>
           </div>
           `;
      const editBtn = movieCard.querySelector("button.edit");
      editBtn.addEventListener("click", e=>{
-          handleMovieEdit({id, title, rating, poster}, movieCard);
+          handleMovieEdit({id, title, rating, poster, genre}, movieCard);
      });
      const deleteBtn = movieCard.querySelector("button.delete");
      deleteBtn.addEventListener("click", e => {
-          handleMovieDelete({id, title, rating, poster}, movieCard);
+          handleMovieDelete({id, title, rating, poster, genre}, movieCard);
           movieCard.remove();
      })
      movieCol.appendChild(movieCard);
 }
 
-const handleMovieDelete = ({id, title, rating, poster}, movieCard) => {
+const handleMovieDelete = ({id, title, rating, poster, genre}, movieCard) => {
      const url = `http://localhost:3000/movies/${id}`;
      const options = {
           method: "DELETE",
@@ -161,6 +170,22 @@ const renderMovies = (movies) => {
 
 const filterMovies = () => {
      const searchInput = document.querySelector('#search');
+     const genreSelect = document.querySelector('#genre');
+     genreSelect.addEventListener('change', e=>{
+          getMovies()
+          .then(movies=> {
+               let movieResult = movies;
+               console.log(e);
+               movieResult = movieResult.filter(movie => {
+                    if (e.target.value==="all") {
+                         return true;
+                    } else {
+                         return movie.genre === e.target.value;
+                    }
+               });
+               renderMovies(movieResult);
+          });
+     });
      searchInput.addEventListener('input', e => {
           getMovies()
                .then(movies=> {
